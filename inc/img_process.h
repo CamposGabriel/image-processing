af6abcdd9a6ba484ad7647524a2110b41068f094
Gabriel Campos <contatogabrielcamposoficial@gmail.com>
2019-05-16 13:27:35 -0300

    Update img_process.h
    Adicionado as descrições das funções.

--- inc/img_process.h
+++ inc/img_process.h
@@ -7,10 +7,13 @@ typedef struct pixel {
   int blue;
 } RGB;
 
+// Abre um arquivo de imagem, dado o nome, localizado na pasta "img" dentro da pasta do projeto.
 void openFile(char name[]);
 
+// Coleta os dados de um arquivo de imagem, dado o nome, localizado na pasta "img" dentro da pasta do projeto.
 RGB* readFileData(char imgType[], int *columnVal, int *rowVal, int *colorVarVal, int *pixelQtdVal);
 
+// Cria um novo arquivo de imagem localizado dentro da pasta "img" com as alterações feitas.
 void createImg(char name[], char imgType[], int columns, int rows, int colorVar, int pixelQtd, RGB pixelVector[]);
 
 #endif
